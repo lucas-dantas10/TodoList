f7d98a3938ba97c35ca158f333dc3da5906c0a7b
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-06-29 16:25:04 -0300

    Separado header e buttons em componentes

---
+++ src/components/Carousel/ButtonCarousel.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="flex justify-between items-center p-8 mt-10">
+        <button @click="swiper.slidePrev()" class="uppercase text-xl text-slate-500">Voltar</button>
+        <button @click="swiper.slideNext()" class="uppercase text-xl border border-indigo-500 rounded-md py-2 px-4 bg-indigo-500">
+            <router-link to="/welcome" v-if="currentNameButton.value == 'Começar'">
+                {{ currentNameButton.value }}
+            </router-link> 
+
+            <p v-else>{{ currentNameButton.value }}</p>
+        </button>
+    </div>
+</template>
+<script>
+import { useSwiper } from 'swiper/vue';
+
+export default {
+    setup() {
+        const swiper = useSwiper();
+
+        return {
+            swiper,
+        };
+    },
+
+    props: {
+        currentNameButton: Object
+    }
+};
+</script>
---
+++ src/components/Carousel/HeaderCarousel.vue
@@ -0,0 +1,30 @@
+<template>
+    <header class="p-6">
+        <ul>
+            <li>
+                <a @click="swiper.slideTo(lastIndex)" href="#" class="text-2xl uppercase text-slate-500">Pular</a>
+            </li>
+        </ul>
+    </header>
+</template>
+
+<script>
+import { useSwiper } from 'swiper/vue';
+
+export default {
+    setup() {
+        const swiper = useSwiper();
+
+        return {
+            swiper
+        }
+    },
+
+    props: {
+        lastIndex: {
+            type: Number,
+            require: true
+        }
+    }
+}
+</script>
