e7a562d65d1e4aa122b0e973a083193fa7c5c170
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-06-30 16:31:24 -0300

    adicionada lib fontawsome

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 import router from './router/index.js';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+
+library.add([
+    faChevronLeft
+]);
 
 const app = createApp(App);
-
-app.use(router)
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.use(router);
 app.mount('#app');
