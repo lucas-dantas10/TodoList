ba7f552a408752eeb0e4ab6d6fb1364387651299
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-07-03 17:18:54 -0300

    Mudança nos botões

--- src/views/Intro/Welcome.vue
+++ src/views/Intro/Welcome.vue
@@ -15,14 +15,17 @@
                 </div>
 
                 <div class="flex flex-col items-center gap-6 w-full">
-                    <button class="uppercase border border-indigo-500 rounded-md py-2 px-4 bg-indigo-500 w-full">
-                        <router-link to="/login">
+                    <router-link to="/login" class="btn-main text-center">
+                        <button class="uppercase">
                             Login
-                        </router-link>
-                    </button>
-                    <button class="uppercase bg-primary border-2 border-indigo-500 rounded-md px-4 py-2 w-full">
-                        Criar Conta
-                    </button>
+                        </button>
+                    </router-link>
+
+                    <router-link to="/criar-conta" class="text-center w-full uppercase bg-primary border-2 border-indigo-500 rounded-md px-4 py-2">
+                        <button class="uppercase">
+                            Criar Conta
+                        </button>
+                    </router-link>                    
                 </div>
             </div>
         </div>
