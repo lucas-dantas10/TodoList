7466954a8bcd5caa5cf7adc376ea0e69eae4f8dd
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-07-07 18:03:50 -0300

    Adicionado state de tasks e categories

--- src/store/state.js
+++ src/store/state.js
@@ -2,5 +2,16 @@ export default {
     user: {
         token: sessionStorage.getItem('TOKEN'),
         data: {}
+    },
+    
+    tasks: {
+        data: {},
+        loading: false,
+        length: 1
+    },
+
+    categories: {
+        data: {},
+        loading: false,
     }
 }
