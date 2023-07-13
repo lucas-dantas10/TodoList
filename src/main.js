446dea4d5cfe80d3af571aacd661e8e6aeb484a3
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-07-13 17:59:47 -0300

    Adicionado mais icones

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { 
     faChevronLeft, faFilter, faHouse, faCalendarDays, faPlus, faClock, faUser, faMagnifyingGlass, faChevronDown,
-    faGraduationCap, faTag
+    faGraduationCap, faTag, faBriefcase, faDumbbell
  } from '@fortawesome/free-solid-svg-icons';
 
 import { faClockFour } from '@fortawesome/free-regular-svg-icons';
@@ -18,7 +18,7 @@ import { faGoogle } from '@fortawesome/free-brands-svg-icons';
  
 library.add([
     faChevronLeft, faGoogle, faFilter, faHouse, faCalendarDays, faPlus, faClock, faUser, faMagnifyingGlass,
-    faChevronDown, faGraduationCap, faClockFour, faTag
+    faChevronDown, faGraduationCap, faClockFour, faTag, faBriefcase, faDumbbell
 ]);
 
 const app = createApp(App);
