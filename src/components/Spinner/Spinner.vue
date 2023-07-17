3ae18aaf0c1d675bebafa9bb15c6c00045dad55b
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-07-17 15:27:01 -0300

    Estilização melhorada para o spinner de full screen

--- src/components/Spinner/Spinner.vue
+++ src/components/Spinner/Spinner.vue
@@ -6,7 +6,7 @@
     </div>
 
     <div v-if="show && fullPage == true" class="w-screen h-screen flex justify-center items-center">
-        <div class="inline-block h-6 w-6 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+        <div class="inline-block h-[4.5rem] w-[4.5rem] animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
             role="status">
         </div>
     </div>
