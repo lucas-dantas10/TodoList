f13cbf475b37947a8de2114d14e8289f1a70d135
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-07-17 12:36:28 -0300

    Alteração na mutation para setar tarefas e adicionada mutation para setar o visited

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,9 +1,23 @@
+export function notify(state, notification) {
+    state.notifications.push(notification);
+
+    setTimeout(() => {
+        state.notifications.pop();
+    }, 3000);
+}
+
 export function setToken(state, token) {
     state.user.token = token;
     if (token) {
         sessionStorage.setItem('TOKEN', token);
     } else {
         sessionStorage.removeItem('TOKEN');
+    }
+}
+
+export function setVisited(state) {
+    if (!localStorage.getItem('VISITED')) {
+        localStorage.setItem('VISITED', true);
     }
 }
 
@@ -21,10 +35,6 @@ export function setTask(state, task) {
     state.tasks.data.push(task);
 }
 
-export function notify(state, notification) {
-    state.notifications.push(notification);
-
-    setTimeout(() => {
-        state.notifications.pop();
-    }, 3000);
+export function setTasks(state, tasks) {
+    state.tasks.data = tasks;
 }
