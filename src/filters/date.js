33476a390e722de0ae4175fe53f4b234d5ce38d6
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-07-17 12:34:17 -0300

    Mudança nos filtros de data para o padrão ISO

--- src/filters/date.js
+++ src/filters/date.js
@@ -3,15 +3,28 @@ export default function dateBR(value) {
     return null;
   }
 
-  const options = {
-    year: "numeric",
-    month: "numeric",
-    day: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric",
-  };
-  return new Date(value).toLocaleString("pt-BR", {
-    options
-  }).replace(/,/g, '');
+  const date = new Date(value);
+
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
+
+  let horas = date.getHours();
+  let minutos = date.getMinutes();
+  let segundos = date.getSeconds();
+  var horaFormatada = ("0" + horas).slice(-2);
+  var minutoFormatado = ("0" + minutos).slice(-2);
+  var segundoFormatado = ("0" + segundos).slice(-2);
+
+  if (month < 10) {
+    month = `0${month}`;
+  }
+
+  if (day < 10) {
+    day = `0${day}`;
+  }
+
+  const dateFormated = `${year}-${month}-${day} ${horaFormatada}:${minutoFormatado}:${segundoFormatado}`
+
+  return dateFormated;
 }
