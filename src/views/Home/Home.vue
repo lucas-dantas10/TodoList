2d957b0cdc85a420e3f775b3e45d0d5733dc526a
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-07-18 17:00:12 -0300

    Ajuste nos filtros da tela home, incompleto

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -24,43 +24,54 @@
                             placeholder="Procure pela sua tarefa...">
                     </div>
 
-                    <div class="w-[30%]">
-                        <div class="w-full flex flex-row px-4 py-2 gap-2 border border-[#363636] bg-[#363636] rounded-lg">
-                            <select class="w-full bg-[#363636] flex flex-row items-center gap-2">
-                                <option value="hoje" selected>
-                                    Hoje
-                                    <font-awesome-icon :icon="['fas', 'chevron-down']" />
-                                </option>
-                                <option value="ontem">Ontem</option>
-                            </select>
-                        </div>
-                    </div>
-
-                    <div class="w-full h-[20%] flex flex-row items-center py-2 px-4 bg-[#363636] rounded-md" v-for="t in tasks">
-                        <div class="w-[15%] h-full flex justify-center items-center">
-                            <input type="radio" class="w-6 h-6">
-                        </div>
-                        <div class="w-full flex flex-col gap-2 items-center">
-                            <div class="w-full">
-                                <p class="text-xl">{{ t.title }}</p>
+                    <div class="w-[100%] h-[100%] flex flex-col gap-[1rem]">
+                        <div class="w-[30%]">
+                            <div
+                                class="w-full flex flex-row px-4 py-2 gap-2 border border-[#363636] bg-[#363636] rounded-lg">
+                                <select class="w-full bg-[#363636] flex flex-row items-center gap-2" v-model.trim="daysOptions" @change="verifyDate()">
+                                    <option value="" selected>
+                                        Todas
+                                        <font-awesome-icon :icon="['fas', 'chevron-down']" />
+                                    </option>
+                                    <option :value="$filters.justDate(new Date())" selected>
+                                        Hoje
+                                    </option>
+                                    <option>
+                                        Ontem
+                                    </option>
+                                </select>
                             </div>
-                            <div class="w-full flex flex-row justify-between items-center">
-                                <div class="text-gray-400">{{ t.date }}</div>
-                                <div class="flex flex-row gap-6">
-                                    <div
-                                        :class="`flex flex-row items-center justify-center gap-2 border-${t.color_category} bg-${t.color_category} rounded-md px-2 py-2`">
-                                        <font-awesome-icon :icon="['fas', 'graduation-cap']" />
-                                        <p>{{t.name_category}}</p>
+                        </div>
+
+                        <div class="w-full h-[20%] flex flex-row items-center py-2 px-4 bg-[#363636] rounded-md"
+                            v-for="t in tasks">
+                            <div class="w-[15%] h-full flex justify-center items-center">
+                                <input type="checkbox" class="w-6 h-6">
+                            </div>
+                            <div class="w-full flex flex-col gap-2 items-center">
+                                <div class="w-full">
+                                    <p class="text-xl">{{ t.title }}</p>
+                                </div>
+                                <div class="w-full flex flex-row justify-between items-center">
+                                    <div class="text-gray-400">{{ $filters.date(t.date) }}</div>
+                                    <div class="flex flex-row gap-6">
+                                        <div
+                                            :class="`flex flex-row items-center justify-center gap-2 border-${t.color_category} bg-${t.color_category} rounded-md px-2 py-2`">
+                                            <font-awesome-icon :icon="['fas', 'graduation-cap']" />
+                                            <p>{{ t.name_category }}</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+
+
                 </div>
             </div>
         </AppLayout>
 
-        
+
     </div>
 </template>
 
@@ -81,12 +92,17 @@ export default {
     data() {
         return {
             searchShow: false,
+            daysOptions: ''
         }
     },
 
     methods: {
         changeShow() {
             this.searchShow = !this.searchShow;
+        },
+
+        verifyDate() {
+            this.$store.dispatch('filterDateOfTasks', this.daysOptions);
         }
     },
 
@@ -97,7 +113,7 @@ export default {
 
         isLoading() {
             return this.$store.state.tasks.loading;
-        }
+        },
     }
 }
 </script>
