115950b0674c1d7da0643cba61ef8f7f47fca137
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-07-27 17:04:53 -0300

    Adicionado mais icones

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { 
     faChevronLeft, faFilter, faHouse, faCalendarDays, faPlus, faClock, faUser, faMagnifyingGlass, faChevronDown,
-    faGraduationCap, faTag, faBriefcase, faDumbbell, faMusic, faHeartPulse
+    faGraduationCap, faTag, faBriefcase, faDumbbell, faMusic, faHeartPulse, faRetweet, faXmark, faPen, faTrash
  } from '@fortawesome/free-solid-svg-icons';
 
 import { faClockFour } from '@fortawesome/free-regular-svg-icons';
@@ -20,7 +20,8 @@ import { faGoogle } from '@fortawesome/free-brands-svg-icons';
  
 library.add([
     faChevronLeft, faGoogle, faFilter, faHouse, faCalendarDays, faPlus, faClock, faUser, faMagnifyingGlass,
-    faChevronDown, faGraduationCap, faClockFour, faTag, faBriefcase, faDumbbell, faMusic, faHeartPulse
+    faChevronDown, faGraduationCap, faClockFour, faTag, faBriefcase, faDumbbell, faMusic, faHeartPulse, faRetweet,
+    faXmark, faPen, faTrash
 ]);
 
 const app = createApp(App);
