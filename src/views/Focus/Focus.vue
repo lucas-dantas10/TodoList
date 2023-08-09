7f88f6849bac7b663395169e7bcf18176f249ab1
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-08-08 23:44:25 -0300

    Criada view de Focus e ja finalizada

---
+++ src/views/Focus/Focus.vue
@@ -0,0 +1,14 @@
+<template>
+  <AppLayout title="Foco" :focus="false">
+    <section class="w-full h-full">
+      <div class="w-full h-full p-6">
+        <stop-watch />
+      </div>
+    </section>
+  </AppLayout>
+</template>
+
+<script setup>
+import AppLayout from "../../components/Layouts/AppLayout.vue";
+import StopWatch from "../../components/StopWatch/StopWatch.vue";
+</script>
