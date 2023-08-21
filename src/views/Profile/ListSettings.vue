df2521e9e716af8e5951db9c691d981b9d78f113
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-08-21 10:37:36 -0300

    Adicionado emit para mudar o boolean do modal, para aparecer ou nao

--- src/views/Profile/ListSettings.vue
+++ src/views/Profile/ListSettings.vue
@@ -3,13 +3,22 @@
     <h4 class="text-gray-500 text-lg">{{ title }}</h4>
 
     <div class="flex flex-col gap-4">
-        <div class="flex items-center justify-between" v-for="(submenu, index) in submenusItems" :key="index">
+        <div 
+            v-for="(submenu, index) in submenusItems" 
+            :key="index" 
+            class="flex items-center justify-between" 
+            @click.prevent="emit('showModal', true)"
+        >
             <div class="flex items-center gap-2">
                 <font-awesome-icon class="text-xl" :icon="['fas', submenu.icon]" />
                 <p class="text-xl">{{ submenu.subtitle }}</p>
             </div>
 
-            <font-awesome-icon class="text-xl" :icon="['fas', 'chevron-right']" />
+            <RouterLink v-if="submenu.href" :to="submenu.href">
+                <font-awesome-icon class="text-xl" :icon="['fas', 'chevron-right']" />
+            </RouterLink>    
+            
+            <font-awesome-icon v-else class="text-xl" :icon="['fas', 'chevron-right']" />
         </div>
     </div>
   </div>
@@ -20,5 +29,9 @@
 const props = defineProps({
     title: String,
     submenusItems: Array
-})
+});
+
+const emit = defineEmits([
+    'showModal'
+]);
 </script>
