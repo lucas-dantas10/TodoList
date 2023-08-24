66f3bb5cc6e7d0c464bff55187cf316d9de5c3b2
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-08-24 11:58:08 -0300

    Adicionado meta de autenticações nas rotas que precisam estar autenticado e adicionada rota para a tela de calendario

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import store from '../store';
 import Category from '../views/Category/Category.vue';
+import { faInternetExplorer } from "@fortawesome/free-brands-svg-icons";
 const Intro = () => import("../views/Intro/Intro.vue");
 const Welcome = () => import("../views/Intro/Welcome.vue");
 const Login = () => import("../views/Login/Login.vue");
@@ -8,6 +9,9 @@ const CreateAccount = () => import('../views/Login/CreateAccount.vue')
 const Home = () => import("../views/Home/Home.vue");
 const Task = () => import("../views/Task/Task.vue");
 const Focus = () => import("../views/Focus/Focus.vue");
+const Profile = () => import("../views/Profile/Profile.vue");
+const Settings = () => import("../views/Profile/Settings/Settings.vue");
+const Calendary = () => import("../views/Calendary/Calendary.vue");
 
 const routes = [
     {
@@ -44,17 +48,50 @@ const routes = [
     {
         name: 'Category',
         path: '/category',
-        component: Category
+        component: Category,
+        meta: {
+            requiresAuth: true
+        }
     },
     {
         name: 'Task',
         path: '/task/:id',
-        component: Task
+        component: Task,
+        meta: {
+            requiresAuth: true
+        }
     },
     {
         name: 'Focus',
         path: '/focus',
-        component: Focus
+        component: Focus,
+        meta: {
+            requiresAuth: true
+        }
+    },
+    {
+        name: 'Profile',
+        path: '/profile',
+        component: Profile,
+        meta: {
+            requiresAuth: true
+        }
+    },
+    {
+        name: 'Profile.Settings',
+        path: '/profile/settings',
+        component: Settings,
+        meta: {
+            requiresAuth: true
+        }
+    },
+    {
+        name: 'Calendary',
+        path: '/calendary',
+        component: Calendary,
+        meta: {
+            requiresAuth: true
+        }
     }
 ];
 
