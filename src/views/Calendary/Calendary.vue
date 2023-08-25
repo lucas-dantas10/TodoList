ad9c08b686b1174de02debe5712b637280b6f241
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-08-25 15:33:53 -0300

    Adicionado layout padrao do app e calendario semanal

--- src/views/Calendary/Calendary.vue
+++ src/views/Calendary/Calendary.vue
@@ -1,5 +1,35 @@
 <template>
-    <div>
-        <h1>CALENDARY</h1>
-    </div>
+    <AppLayout title="Calendário">
+        <div class="px-6">
+            <section>
+                <VDatePicker
+                    v-model="date"
+                    expanded
+                    view="weekly"
+                    :is-dark="true"
+                    borderless
+                    transparent
+                    mode="date"
+                    @dayclick="formatDate()"
+                />
+            </section>
+
+            <section class="mt-8">
+                <h2>Sessão 3</h2>
+            </section>
+        </div>
+    </AppLayout>
 </template>
+
+<script setup>
+import AppLayout from "../../components/Layouts/AppLayout.vue";
+import justDate from "../../filters/justDate.js";
+import { ref } from "vue";
+
+const date = ref(new Date());
+
+function formatDate() {
+    const dateFormated = justDate(date.value);
+    console.log(dateFormated);
+}
+</script>
