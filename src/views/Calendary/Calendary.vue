30109105a76ea2585f02ddc3c31b4aad2c0aa46c
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-08-31 14:26:12 -0300

    Adicionada lógica de pegar as tasks por data

--- src/views/Calendary/Calendary.vue
+++ src/views/Calendary/Calendary.vue
@@ -15,21 +15,44 @@
             </section>
 
             <section class="mt-8">
-                <h2>Sessão 3</h2>
+                <Tasks 
+                    v-if="tasksSelect()"
+                    select-is="notCompleted"
+                />
             </section>
         </div>
     </AppLayout>
 </template>
 
 <script setup>
+import { useStore } from "vuex";
 import AppLayout from "../../components/Layouts/AppLayout.vue";
 import justDate from "../../filters/justDate.js";
-import { ref } from "vue";
+import Tasks from '../Home/Tasks.vue';
+import { ref, computed } from "vue";
 
 const date = ref(new Date());
+const store = useStore();
+
+const allTasks = computed(() => store.state.tasks.data);
 
 function formatDate() {
     const dateFormated = justDate(date.value);
-    console.log(dateFormated);
+}
+
+function tasksSelect() {
+    const dateFormated = justDate(date.value);
+
+    const tasksForDate = allTasks.value.filter(task => {
+        let dateFormatedTask = justDate(task.date);
+        const currentTask = dateFormatedTask == dateFormated ? task : false;
+        return currentTask;
+    });
+
+    if (tasksForDate.length == 0) {
+        return false;
+    } else {
+        return tasksForDate;
+    }
 }
 </script>
