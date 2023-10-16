94c5fbe666cdd6224435ec1d6e00477d8db79b95
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-10-16 10:27:41 -0300

    Mudança no baseURL e libs

--- src/axios.js
+++ src/axios.js
@@ -3,14 +3,15 @@ import store from './store';
 import router from './router/index.js';
 
 const axiosClient = axios.create({
-    baseURL: 'https://tolistapi.000webhostapp.com/api', // PRODUCTION
+    baseURL: "https://omlyy.com.br/api", // PRODUCTION
     //baseURL: 'http://localhost:8080/api', // ENVIRMONT
 });
 
-// axiosClient.defaults.withCredentials = true;
+axiosClient.interceptors.request.use((config) => {
+    const token = store.state.user.token;
 
-axiosClient.interceptors.request.use(config => {
-    config.headers.Authorization = `Bearer ${store.state.user.token}`
+    config.headers.Authorization = `Bearer ${token}`;
+    // config.headers["Content-Type"] = "application/json";
     return config;
 });
 
@@ -21,7 +22,6 @@ axiosClient.interceptors.response.use(response => {
         store.commit('setToken', null);
         router.push({name: 'Login'});
     }
-    
     throw error;
 });
 
